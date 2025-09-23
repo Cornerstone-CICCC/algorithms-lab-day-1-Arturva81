da758bb3b928347d54b049a294dedef8e740c13f
Arturo Vazquez <arturva.81@gmail.com>
2025-09-22 19:00:07 -0700

    Completed exercise 8

---
+++ ex08.js
@@ -0,0 +1,12 @@
+function longestWord(text) {
+  let words = text.split(' ');
+    let longest = '';
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longest.length) {
+            longest = words[i];
+        }
+    }
+    return longest;
+}
+
+console.log(longestWord('The duck loves bananas and grapes')) // 'bananas'
